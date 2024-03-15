f496f29a41ba92215cd92f506c7268cf54f44b48
Jorge Rivera <jorge.rivera@ssde.com.mx>
2024-03-15 10:46:22 -0600

    SFG-MVC-04. Added test case for Not found customer

--- src/test/java/com/ssde/tutorials/restmvc/controller/CustomerControllerIT.java
+++ src/test/java/com/ssde/tutorials/restmvc/controller/CustomerControllerIT.java
@@ -10,8 +10,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Created by jra, SSDE, Inc
@@ -51,5 +53,10 @@ class CustomerControllerIT {
         CustomerDTO dto = customerController.getCustomerById(customer.getId());
 
         assertThat(dto).isNotNull();
+    }
+
+    @Test
+    void testCustomerIdNotFound() {
+        assertThrows(NotFoundException.class, () -> customerController.getCustomerById(UUID.randomUUID()));
     }
 }
